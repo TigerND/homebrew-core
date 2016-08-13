f6ec0fd95fa284fb81579f3f834a758ada2dd2b8
Aleksandr Zykov <tiger@plexrayinc.com>
2016-08-13 22:10:16 +0200

    steem 0.13.0-rc3

--- Formula/steem.rb
+++ Formula/steem.rb
@@ -2,8 +2,8 @@ class Steem < Formula
   desc "Proof of Work blockchain with an unproven consensus algorithm."
   homepage "https://steemit.com/"
   url "https://github.com/steemit/steem.git",
-      :tag => "v0.12.2",
-      :revision => "b551cc7934f39b9f92151c6e414a397cb40647fd"
+      :tag => "0.13.0-rc3",
+      :revision => "6958b0c6cce693684a24a7594b0b056f9dbcd234"
 
   depends_on :macos => [:yosemite, :el_capitan, :sierra]
 
@@ -15,7 +15,6 @@ class Steem < Formula
   depends_on "doxygen" => :build
   depends_on "berkeley-db" => :optional
   depends_on "gperftools" => :optional
-  depends_on "qt5" => :optional
   depends_on "icu4c"
   depends_on "boost"
   depends_on "openssl"
@@ -24,10 +23,10 @@ class Steem < Formula
   def install
     cmake_args = %W[
       CMakeLists.txt
-      -DLOW_MEMORY_NODE=ON
+      -DCMAKE_BUILD_TYPE=Release
+      -DLOW_MEMORY_NODE=OFF
+      -DENABLE_CONTENT_PATCHING=ON
     ]
-
-    cmake_args << "-DENABLE_CONTENT_PATCHING=OFF" if build.without? "qt5"
 
     system "cmake", *(cmake_args + std_cmake_args)
     system "make", "install"
